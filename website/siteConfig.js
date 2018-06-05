c320f1ec4b9bd7293eead5aa94ee873944b407aa
Joel Marcey <joelm@fb.com>
2018-06-04 17:13:01 -0700

    Use https in siteconfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,7 +11,7 @@
 const siteConfig = {
   title: 'Hack' /* title for your website */,
   tagline: 'Programming Productivity Without Breaking Things',
-  url: 'http://hacklang.org' /* your website url */,
+  url: 'https://hacklang.org' /* your website url */,
   baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
