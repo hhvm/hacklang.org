0c4cc3923e947ee88b31c63fa80d3d57b9f3317e
Andrew DiMola <dimola@fb.com>
2021-05-02 21:54:18 -0400

    Reformat Async example

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -15,17 +15,26 @@ const translate = require("../../server/translate.js").translate;
 
 const pre = "```";
 const asyncExample= `${pre}hack
-  async function hello_world(): Awaitable<string> {
-    return "Hello World"; // simple, trivial example
+<<__EntryPoint>>
+  async function my_example(): Awaitable<void> {
+    $user_ids = vec[1, 2, 3];
+
+    // Initiate all the database requests together,
+    // so we spend less time waiting.
+    $user_names = await Vec\map_async(
+      $user_ids,
+      async ($id) ==> await fetch_user_name($id),
+    );
+    // Execution continues after requests complete.
+
+    echo Str\join($user_names, ", ");
   }
 
-  <<__EntryPoint>>
-  async function call_hello_world(): Awaitable<void> {
-
-  $aw = hello_world();
-  $result = await $aw; // Suspends until $aw completes
-  echo $result; // "Hello World"
-  }
+  async function fetch_user_name(int $_):
+    Awaitable<string> {
+      // This could be a database request.
+      return "";
+    }
 ${pre}`;
 const genericsExample = `${pre}hack
   class Box<T> {
