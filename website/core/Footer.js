07419547ada9bb6ec3792423e560f091fe4fb109
Andrew DiMola <dimola@fb.com>
2021-05-10 15:33:10 -0400

    Remove /home footer
    Redundant / Not visible

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -23,16 +23,6 @@ class Footer extends React.Component {
     return (
       <footer className="nav-footer" id="footer">
         <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
-          </a>
           <a
             className="github-button"
             href={this.props.config.repoUrl}
